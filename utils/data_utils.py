a3d6ddf520f5a12d88c0268b54dfa0dbfa681dc0
Batuhan Ozcomlekci <b.ozcomlekci@ug.bilkent.edu.tr>
2022-05-06 14:26:13 +0300

    Area Graph Completed

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,13 +1,14 @@
-from data.dataset import INT_TO_LABEL
+from data.dataset import INT_TO_LABEL, LABEL_TO_HSL
 
 def ratio_to_category(collection):
     plot_array = []
     nOfImages  = len(collection)
     nOfLabels = 7
-    labels = list(INT_TO_LABEL.keys())
+    labels = list(INT_TO_LABEL.values())
     print(labels)
     for i in range(nOfLabels):
-        plot_array.append({'id': INT_TO_LABEL[labels[i]]})
+        plot_array.append({'id': labels[i]})
+        plot_array[i]['color'] = LABEL_TO_HSL[labels[i]]
         plot_array[i]['data'] = []
         for j,item in enumerate(collection):
             plot_array[i]['data'].append({'x':j, 'y':item['ratio_dict'][i]['value']})
@@ -18,6 +19,3 @@ def get_images(collection):
     for item in collection:
         imgs.append(item['img'])
     return imgs
-
-#{id: 'Background', data: [{ x: 1997, y:1 },{ x: 28:14, y:1 },{ x: 28:14, y:1 },{ x: 28:14, y:1 }]}'''
-#[imgs:[img1, img2,img3], plot_array:[{...}, {...}, {...}, {...}, {...}, {...}, {...}]]
